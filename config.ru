878bfe7cae18ad3bc59765713dd7f68aef088e5b
poh <pohchiat@gmail.com>
2014-11-19 16:48:17 +0900

    add config files for damoenizing

---
+++ config.ru
@@ -0,0 +1,9 @@
+require "rubygems"
+require "sinatra"
+# require './kuromoji'
+# require_relative 'kuromoji'
+require "/home/poh/Projects/kuromoji-web/kuromoji.rb"
+
+# require File.expand_path '../kuromoji.rb', __FILE__
+
+run KuromojiApp
--- kuromoji.rb
+++ kuromoji.rb
@@ -1,71 +1,75 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/json'
 require 'json'
 require 'pp'
 
-before do
-  content_type :html, 'charset' => 'utf-8'
-end
+class KuromojiApp < Sinatra::Base
+  DIRECTORY = "/home/poh/Projects/kuromoji-web/"
 
-def translate_type(type)
-  if type == "動詞"
-    "verb"
-  elsif type == "名詞"
-    "noun"
-  else
-    "others"
+  before do
+    content_type :html, 'charset' => 'utf-8'
   end
-end
 
-def parse_token(token)
-  split_token = token.split("\t")
-  token_name = split_token[0]
-
-  attributes = split_token[1].split(",")
-  word_type = attributes[0]
-  dict_form = attributes[6]
-
-  result = {}
-  result["plain"] = dict_form
-  result["tag"] = translate_type(word_type)
-  result["original"] = token_name
-
-  result
-end
-
-post '/' do
-  content_type :json
-
-  # parse form-data and call command
-  input_json = JSON.parse(params["input"])
-  command = "java -Dfile.encoding=UTF-8 -cp kuromoji-0.7.7/lib/kuromoji-0.7.7.jar:kuromoji-0.7.7/src/main/java org.atilika.kuromoji.example.TokenizerExample "
-  input_json.each do |sen|
-    sen = "\"#{sen}\" "
-    command << sen
+  def translate_type(type)
+    if type == "動詞"
+      "verb"
+    elsif type == "名詞"
+      "noun"
+    else
+      "others"
+    end
   end
-  output = `#{command}`
 
-  # split by sentences
-  sentences = output.split("sentence:\n")
-  sentences = sentences[1..sentences.size]
+  def parse_token(token)
+    split_token = token.split("\t")
+    token_name = split_token[0]
 
-  # process each sentences
-  overall_json = []
-  sentences.each do |sentence|
-    tokens = sentence.split("\n")
+    attributes = split_token[1].split(",")
+    word_type = attributes[0]
+    dict_form = attributes[6]
 
-    result_json = {}
-    result_json["input"] = tokens[0]
-    parsed_tokens = []
-    tokens = tokens[1..tokens.size]
-    tokens.each do |token|
-      parsed_token = parse_token(token)
-      parsed_tokens << parsed_token
+    result = {}
+    result["plain"] = dict_form
+    result["tag"] = translate_type(word_type)
+    result["original"] = token_name
+
+    result
+  end
+
+  post '/' do
+    content_type :json
+
+    # parse form-data and call command
+    input_json = JSON.parse(params["input"])
+    command = "java -Dfile.encoding=UTF-8 -cp #{DIRECTORY}kuromoji-0.7.7/lib/kuromoji-0.7.7.jar:#{DIRECTORY}kuromoji-0.7.7/src/main/java org.atilika.kuromoji.example.TokenizerExample "
+    input_json.each do |sen|
+      sen = "\"#{sen}\" "
+      command << sen
+    end
+    output = `#{command}`
+
+    # split by sentences
+    sentences = output.split("sentence:\n")
+    sentences = sentences[1..sentences.size]
+
+    # process each sentences
+    overall_json = []
+    sentences.each do |sentence|
+      tokens = sentence.split("\n")
+
+      result_json = {}
+      result_json["input"] = tokens[0]
+      parsed_tokens = []
+      tokens = tokens[1..tokens.size]
+      tokens.each do |token|
+        parsed_token = parse_token(token)
+        parsed_tokens << parsed_token
+      end
+
+      result_json["tokens"] = parsed_tokens
+      overall_json << result_json
     end
 
-    result_json["tokens"] = parsed_tokens
-    overall_json << result_json
+    json overall_json
   end
-
-  json overall_json
 end
